e03313128ea702e1e1fbba0b004172f8aee46418
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2021-12-08 15:54:01 -0600

    Finish highlighting of face zones

---
+++ libs/nueva-consulta/feature/src/lib/face-interactive-diagram/zones-diagram/zones-diagram.component.ts
@@ -0,0 +1,41 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'consultas-zones-diagram',
+  templateUrl: './zones-diagram.component.html',
+  styleUrls: ['./zones-diagram.component.scss']
+})
+export class ZonesDiagramComponent implements OnInit {
+  @Input() highlights: Observable<string[]> = of([]);
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.highlights.subscribe((highlights) => {
+      //Changits SON LOS MEJORES UwU
+      //Primero, deseleccionar cualquier zona actualmente seleccionada
+      //Después, si el array contiene elementos, seleccionar esos elementos
+      //Si el array no tenía elementos, no seleccionar nada
+      const elements = document.getElementsByClassName('selected');
+      for(let i = 0; i < elements.length; i++){
+        elements[i].classList.remove('selected')
+      }
+      if(highlights.length > 0){
+        const toSelect = [];
+        for(let b = 0; b < highlights.length; b++){
+          const e = document.getElementById(highlights[b]);
+          
+          if(e) toSelect.push(e);
+        }
+        for(const el of toSelect){
+          el.classList.add('selected');
+        }
+        console.log("hi!");
+      }
+
+      
+    })
+  }
+
+}
