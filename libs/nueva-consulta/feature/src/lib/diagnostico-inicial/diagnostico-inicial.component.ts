4d2f604e907a0d7799e6ab7669c32881796b386a
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2021-12-08 09:53:20 -0600

    Added validations to the first step

--- libs/nueva-consulta/feature/src/lib/diagnostico-inicial/diagnostico-inicial.component.ts
+++ libs/nueva-consulta/feature/src/lib/diagnostico-inicial/diagnostico-inicial.component.ts
@@ -1,18 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { ConsultasState, loadEstigmas } from '../..';
+import { select, Store } from '@ngrx/store';
+import { combineLatest } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { ConsultasState, getDiagnosticoMedico, getSignosSintomas, loadEstigmas } from '../..';
 
 @Component({
   selector: 'consultas-diagnostico-inicial',
   templateUrl: './diagnostico-inicial.component.html',
   styleUrls: ['./diagnostico-inicial.component.scss']
 })
-export class DiagnosticoInicialComponent{
+export class DiagnosticoInicialComponent implements OnInit{
+  enableNext = false;
   constructor(private router: Router, private store: Store<ConsultasState>){}
+
+  ngOnInit(): void {
+    combineLatest([this.store.pipe(select(getSignosSintomas)), this.store.pipe(select(getDiagnosticoMedico))]).pipe(tap(([signosSintomas, diagnosticoMedico]) => {
+      if(signosSintomas.length > 0 && diagnosticoMedico.length > 0){
+        this.enableNext = true;
+      }else{
+        this.enableNext = false;
+      }
+    })).subscribe();
+  }
+
   onNextClicked(){
     this.store.dispatch(loadEstigmas());
     this.router.navigateByUrl('/new/tratamientos');
   }
+
+
 
 }
