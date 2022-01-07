a7f8fa78824812ffb3177cd821b5488cfba35176
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2022-01-07 13:12:15 -0600

    Finish the ItemProductoAplicado component

--- libs/nueva-consulta/feature/src/lib/trabajo/item-producto-aplicado/item-producto-aplicado.component.ts
+++ libs/nueva-consulta/feature/src/lib/trabajo/item-producto-aplicado/item-producto-aplicado.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ProductoConsulta } from '@fullstack-angular-nest/nueva-consulta/data-access';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { AplicacionProducto, ProductoConsulta } from '@fullstack-angular-nest/nueva-consulta/data-access';
 
 @Component({
   selector: 'consultas-item-producto-aplicado',
@@ -9,11 +10,39 @@ import { ProductoConsulta } from '@fullstack-angular-nest/nueva-consulta/data-ac
 export class ItemProductoAplicadoComponent implements OnInit {
   @Input() producto!: ProductoConsulta;
 
+  @Output() proximaAplicacionChange = new EventEmitter<string>();
+  @Output() editarAplicacionesClick = new EventEmitter<{producto: ProductoConsulta}>();
+  @Output() aplicacionDiscard = new EventEmitter<{aplicacion: AplicacionProducto, producto: ProductoConsulta}>();
+
   specifyProximaAplicacion = false;
+  showAplicaciones = false;
+
+  proximaAplicacionControl = new FormControl('');
 
   constructor() { }
 
   ngOnInit(): void {
+    this.proximaAplicacionControl.valueChanges.subscribe((value) => {
+      this.proximaAplicacionChange.emit(value);
+    })
+  }
+
+  onEspecificarProximaAplicacionClicked(){
+    this.specifyProximaAplicacion = true;
+    this.proximaAplicacionChange.emit('');
+  }
+
+  onEliminarProximaAplicacionClicked(){
+    this.specifyProximaAplicacion = false;
+    this.proximaAplicacionChange.emit(undefined);
+  }
+
+  onDiscardAplicacionClicked(aplicacion: AplicacionProducto) {
+    this.aplicacionDiscard.emit({aplicacion, producto: this.producto})
+  }
+
+  onEditarAplicacionesClicked(){
+    this.editarAplicacionesClick.emit({producto: this.producto});
   }
 
 }
