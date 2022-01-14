556997239f74a48c229bf7cb6e7cc92cad11753a
Yiz <68924783+TheCodeKat@users.noreply.github.com>
2022-01-14 11:54:34 -0600

    read the comments from the store

--- libs/nueva-consulta/feature/src/lib/diagnostico-inicial/comentarios/comentarios.component.ts
+++ libs/nueva-consulta/feature/src/lib/diagnostico-inicial/comentarios/comentarios.component.ts
@@ -3,6 +3,7 @@ import { FormControl } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { setComentarios } from '../../state/consultas/consultas.actions';
 import { ConsultasState } from '../../state/consultas/consultas.reducer';
+import { getComentarios } from '../../state/consultas/consultas.selectors';
 
 @Component({
   selector: 'consultas-comentarios',
@@ -12,10 +13,21 @@ import { ConsultasState } from '../../state/consultas/consultas.reducer';
 export class ComentariosComponent implements OnInit{
   formControl: FormControl = new FormControl('');
 
+  comentarios = '';
+
   constructor(private store: Store<ConsultasState>) { }
 
   ngOnInit(): void {
-      this.formControl.valueChanges.subscribe((comentarios) => this.store.dispatch(setComentarios({comentarios})))
+    this.formControl.valueChanges.subscribe((comentarios) => {
+      if(this.comentarios !== comentarios){
+        this.store.dispatch(setComentarios({comentarios}))
+      }
+    })
+
+    this.store.select(getComentarios).subscribe(comentarios => {
+      this.formControl.setValue(comentarios);
+    })
+
   }
 
 }
